63a603915ea2e658ce35ddd6dd47d0da3dda7fce
Amit <prerna.amit.jain@gmail.com>
2024-08-31 10:26:08 +0530

    Intital version of project

---
+++ src/main/java/com/learn/thread/ThreadLearningApplication.java
@@ -0,0 +1,13 @@
+package com.learn.thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadLearningApplication.class, args);
+	}
+
+}
