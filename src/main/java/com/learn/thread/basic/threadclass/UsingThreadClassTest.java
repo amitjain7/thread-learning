a2e5072a435a3bcb793a8c08b03c1ef2e7bdb3be
amit <prerna.amit.jain@gmail.com>
2025-04-30 08:25:02 +0530

    add

--- src/main/java/com/learn/thread/basic/threadclass/UsingThreadClassTest.java
+++ src/main/java/com/learn/thread/basic/threadclass/UsingThreadClassTest.java
@@ -8,6 +8,8 @@ public class UsingThreadClassTest {
         log.info("Main Start = > "+Thread.currentThread());
         UsingThreadClass t=new UsingThreadClass();
         t.start();
+        UsingThreadClass t2=new UsingThreadClass();
+        t2.start();
         log.info("Main End = > "+Thread.currentThread());
     }
 }
